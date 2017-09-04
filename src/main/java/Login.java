6f91fe434d546aa435eeb2c4a15ebb0d6029c6b9
Abhijit <Abhijit@users.github.com>
2017-09-05 01:07:54 +0530

    code formatting done

--- src/main/java/Login.java
+++ src/main/java/Login.java
@@ -1,3 +1,4 @@
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -16,115 +17,80 @@ import java.sql.*;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 
-
 /**
  *
  * @author Abhijit
  */
-@WebServlet(urlPatterns = {"/Login"})
+@WebServlet(urlPatterns = { "/Login" })
 public class Login extends HttpServlet {
-
-       static Connection con = null;
+	static Connection con = null;
 	static PreparedStatement ps = null;
-    
-    static String op;
-    
-   
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException 
-    {
-        
-        String output="";
-        response.setContentType("text/html;charset=UTF-8");
-         try (PrintWriter out = response.getWriter()) 
-            {
-                String email=request.getParameter("email");
-                String pwd=request.getParameter("pwd");
-                
-                if(checkLogin(email,pwd))
-                    output="Login Successful.  Hello "+op;
-                else
-                    output="Login Failed. Invalid Email or Password!";
-         
-                 request.setAttribute("output", output);
-               request.getRequestDispatcher("final.jsp").forward(request, response);
-                
-                
-           
-                
-            }
-          
-    }
+	static String op;
 
-   
-    
-     public static boolean checkLogin(String email,String pwd)
-    {
-       boolean st =false;
-      try{
+	protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
-	 //loading drivers for mysql
-         Class.forName("com.mysql.jdbc.Driver");
+		String output = "";
+		response.setContentType("text/html;charset=UTF-8");
+		try (PrintWriter out = response.getWriter()) {
+			String email = request.getParameter("email");
+			String pwd = request.getParameter("pwd");
 
- 	 //creating connection with the database 
-         con=DriverManager.getConnection("jdbc:mysql://sql12.freemysqlhosting.net:3306/sql12192611", "sql12192611", "XWch1xppLf");
-			 ps =con.prepareStatement
-                             ("select * from temp where email=? and pwd=? and verify=?");
-         ps.setString(1, email);
-         ps.setString(2, pwd);
-         ps.setString(3, "1");
-         
-         
-         
-         ResultSet rs =ps.executeQuery();
-         st = rs.next();
-         op=rs.getString("fname");
-         
-         }
-      catch(Exception e)
-      {
-          e.printStackTrace();
-      }
-        
-        
-        return st;
-    }
-    
-   
- 
-    
-    
-   
-        
- 
-    
-   
-    
-    
-    
-    
-    
-    
-   
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-        
-        
-        
-    }
+			if (checkLogin(email, pwd)) {
+				output = "Login Successful.  Hello ".concat(op);
+			} else {
+				output = "Login Failed. Invalid Email or Password!";
+			}
 
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
+			request.setAttribute("output", output);
+			request.getRequestDispatcher("final.jsp").forward(request, response);
 
-  
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }
+		}
+
+	}
+
+	public static boolean checkLogin(String email, String pwd) {
+		boolean st = false;
+		try {
+
+			// loading drivers for mysql
+			Class.forName("com.mysql.jdbc.Driver");
+
+			// creating connection with the database
+			con = DriverManager.getConnection("jdbc:mysql://sql12.freemysqlhosting.net:3306/sql12192611", "sql12192611",
+					"XWch1xppLf");
+			ps = con.prepareStatement("select * from temp where email=? and pwd=? and verify=?");
+			ps.setString(1, email);
+			ps.setString(2, pwd);
+			ps.setString(3, "1");
+
+			ResultSet rs = ps.executeQuery();
+			st = rs.next();
+			op = rs.getString("fname");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return st;
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		processRequest(request, response);
+
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		processRequest(request, response);
+	}
+
+	@Override
+	public String getServletInfo() {
+		return "Short description";
+	}
 
 }
--- src/main/java/Register.java
+++ src/main/java/Register.java
@@ -1,3 +1,4 @@
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -12,7 +13,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
 import java.sql.Connection;
 import java.sql.SQLException;
 
@@ -20,191 +20,110 @@ import java.sql.*;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 
-
 /**
  *
  * @author Abhijit
  */
-@WebServlet(urlPatterns = {"/Register"})
+@WebServlet(urlPatterns = { "/Register" })
 public class Register extends HttpServlet {
-     
-      static Connection Conn = null;
-      static PreparedStatement PrepareStat = null;
-      static PreparedStatement ps = null;
-     
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            
-           // out.println("Abhijit");
-            
-           String st=null,op=null,rt,jdbcSt = null;
-           
-           
-                 String fname = request.getParameter("fname");
-                 String lname = request.getParameter("lname");
-                 String email = request.getParameter("email");
-                 String phn = request.getParameter("phn");
-                 String pwd = request.getParameter("pwd");
-                 
-                 
-                 
-                JDBCConnection(st);
-                
-             if(st!=null)
-             {  jdbcSt =JDBCConnection(st);
-                 out.println("JDBC CONNECTION ERROR");
-             }
-             
-                 //registration(fname, lname, email, phn, pwd,op);
-               //  out.println( registration(fname, lname, email, phn, pwd,op) );
-                 rt = registration(fname, lname, email, phn, pwd,op);
-           
-         
-          //   linkSendStatus = SendVerification.SendMail(email);
-                 
-            request.setAttribute("rt", rt);
-            request.getRequestDispatcher("final.jsp").forward(request, response);
-             
-            /* request.setAttribute("jdbcSt", jdbcSt);
-             request.getRequestDispatcher("final.jsp").forward(request, response);
-            
-            request.setAttribute("registerStatus", registerStatus);
-            request.getRequestDispatcher("final.jsp").forward(request, response);
-            
-            if(flag==1)
-              request.setAttribute("registerStatus", "register");
-            request.getRequestDispatcher("final.jsp").forward(request, response);
-              */
-             
-        }
-    }
+	private static final long serialVersionUID = 4909124230502332027L;
+	static Connection Conn = null;
+	static PreparedStatement PrepareStat = null;
+	static PreparedStatement ps = null;
 
-       
-     private static String JDBCConnection(String st) 
-        {
-          
-		try 
-                    {
-			Class.forName("com.mysql.jdbc.Driver");
-		    } 
-          
-                catch (ClassNotFoundException e)
-                    {
-			e.printStackTrace();
-			
-                    }
- 
-		try 
-                    {
-			
-			Conn = DriverManager.getConnection("jdbc:mysql://sql12.freemysqlhosting.net:3306/sql12192611", "sql12192611", "XWch1xppLf");
-			if (Conn != null)
-                             {
-                                                                                      /*-------Connection Successful*/
-                             } 
-                        
-                        else {
-                            st="Connection=null! [IN TRY]";
-                             }
-                    }
-                catch (SQLException e) 
-                    {
-			e.printStackTrace();
-                          st="ERROR! [IN CATCH]" ;
-                       
-                    }
-                return st;
-                       
-	}
- 
-    
-    
-    private static String registration(String fname, String lname, String email, String phn, String pwd, String op) 
-        {
- 
-            
-       boolean stx =false;
-      try{
-
-			 ps =Conn.prepareStatement
-                             ("select * from temp where email=?");
-         ps.setString(1, email);
-         
-         
-         
-         ResultSet rs =ps.executeQuery();
-         stx = rs.next();
-         
-         }
-      catch(SQLException e)
-      {
-          e.printStackTrace();
-      }
-            
-         if(stx){
-                op="Email Already registered. Please Log In.";
-           
-            return op;
-                    }
-            
-         else{
-             
-           SendVerification.SendMail(email);
-                
-            try {
-                    String insertS = "INSERT  INTO  temp  VALUES  (?,?,?,?,?,?)";
- 
-			PrepareStat = Conn.prepareStatement(insertS);
-			
-                        PrepareStat.setString(1, fname);
-			PrepareStat.setString(2, lname);
-			PrepareStat.setString(3, email);
-			PrepareStat.setString(4, phn);
-			PrepareStat.setString(5, pwd);
-			PrepareStat.setString(6, "0");
-		
-                        PrepareStat.executeUpdate();
-                       op="A Activation Mail is send to your e-mail id. Check your e-mail and click on the link to Activate Your Registration.";
-                   
-                }
-            catch (SQLException e) 
-                {
-			e.printStackTrace();
-                        op="Error!";
-                      
+	protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		try (PrintWriter out = response.getWriter()) {
+			String st = null, op = null, rt, jdbcSt = null;
+			String fname = request.getParameter("fname");
+			String lname = request.getParameter("lname");
+			String email = request.getParameter("email");
+			String phn = request.getParameter("phn");
+			String pwd = request.getParameter("pwd");
+			JDBCConnection(st);
+			if (st != null) {
+				jdbcSt = JDBCConnection(st);
+				out.println("JDBC CONNECTION ERROR");
+			}
+		rt = registration(fname, lname, email, phn, pwd, op);
+			request.setAttribute("rt", rt);
+			request.getRequestDispatcher("final.jsp").forward(request, response);
 		}
-             }
-            return op;
-	
-        }
-        
-        
- 
-   
-    
-    
-    
-    
-    
-    
-  
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
+	}
+	private static String JDBCConnection(String st) {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		}catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		try {
+	Conn = DriverManager.getConnection("jdbc:mysql://sql12.freemysqlhosting.net:3306/sql12192611",
+					"sql12192611", "XWch1xppLf");
+			if (Conn != null) {
+			}else {
+ 				  st = "Connection=null! [IN TRY]";
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			st = "ERROR! [IN CATCH]";
+		}
+		return st;
+	}
 
-   @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
+	private static String registration(String fname, String lname, String email, String phn, String pwd, String op) {
+		boolean stx = false;
+		try {
 
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
+			ps = Conn.prepareStatement("select * from temp where email=?");
+			ps.setString(1, email);
+
+			ResultSet rs = ps.executeQuery();
+			stx = rs.next();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		if (stx) {
+			op = "Email Already registered. Please Log In.";
+			return op;
+		}else {
+			SendVerification.SendMail(email);
+			try {
+				String insertS = "INSERT  INTO  temp  VALUES  (?,?,?,?,?,?)";
+				PrepareStat = Conn.prepareStatement(insertS);
+				PrepareStat.setString(1, fname);
+				PrepareStat.setString(2, lname);
+				PrepareStat.setString(3, email);
+				PrepareStat.setString(4, phn);
+				PrepareStat.setString(5, pwd);
+				PrepareStat.setString(6, "0");
+				PrepareStat.executeUpdate();
+				op = "A Activation Mail is send to your e-mail id. Check your e-mail and click on the link to Activate Your Registration.";
+				} catch (SQLException e) {
+				e.printStackTrace();
+				op = "Error!";
+			    }
+		}
+		return op;
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		processRequest(request, response);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		processRequest(request, response);
+	}
+
+	@Override
+	public String getServletInfo() {
+		return "Short description";
+	}
 
 }
